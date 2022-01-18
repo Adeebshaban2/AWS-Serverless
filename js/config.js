1475cad14a9ce19997aa694d4d24915d298fa42a
Adeebshaban2 <94074337+Adeebshaban2@users.noreply.github.com>
2022-01-19 00:30:59 +0200

    Add files via upload
    new

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_FeNclUtnf', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1e5v6v4l0gm75qpqacv67c777n', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://gbbyv4srh6.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
