4d6ddceae710c8c51e48c58c4ba723c6ac6a0628
Adeebshaban2 <94074337+Adeebshaban2@users.noreply.github.com>
2022-01-19 18:48:58 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_OcJS72gdI', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '63pv0p05tpc5p5vket9i4210rd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '5h075m04l65doidhd7cs0mun40', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
